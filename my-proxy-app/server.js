0204487ea667680a7303cd34988420deddbd3934
ziziidesu <scratch.ziziidesu.scratch@gmail.com>
2024-10-19 21:07:52 +0900

    Update server.js

--- my-proxy-app/server.js
+++ my-proxy-app/server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+// プロキシ設定
+app.use('/', createProxyMiddleware({
+  target: 'https://blogbooks.net', // プロキシしたいURLを指定
+  changeOrigin: true,
+}));
+
+// ポート指定
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Proxy server running on port ${PORT}`);
+});
 
